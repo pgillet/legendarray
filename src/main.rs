0600abb9927813cdb9a43c40616ae0530997171c
Pascal Gillet <pascal.gillet@stack-labs.com>
2023-07-09 17:03:34 +0200

    Use magic numbers

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ where
     data: Vec<T>,
     shape: Vec<usize>,
     dimensions: usize,
+    magic_numbers: Vec<(u32, usize)>
 }
 
 impl<T> MortonArray<T>
@@ -18,10 +19,12 @@ where
         let dimensions = shape.len();
         let size: usize = shape.iter().product();
         let data = Vec::with_capacity(size);
+        let magic_numbers   = generate_magic_numbers(dimensions);
         Self {
             data,
             shape,
             dimensions,
+            magic_numbers
         }
     }
 
@@ -63,71 +66,44 @@ where
             if index > max_index[d] {
                 return None;
             }
-            morton_index |= morton_encode(index as u32) << d;
+            morton_index |= self.morton_encode(index as u32) << d;
         }
         // TODO
         Some(morton_index.try_into().unwrap())
     }
+
+    fn morton_encode(&self, x: u32) -> usize {
+        let mut word: usize = x as usize;
+
+        for magic_number in self.magic_numbers.iter().rev() {
+            word = (word | (word << magic_number.0)) & magic_number.1;
+        }
+        word
+    }
+
+    
 }
 
-fn morton_encode(x: u32) -> usize {
-    let mut word: usize = x as usize;
-    // 64-bits coordiantes for 1D array
-    // word = (word | (word << 0)) & 0xFFFFFFFFFFFFFFFF;
 
-    // 32-bits coordinates for 2D array
-    // word = (word | (word << 32)) & 0x00000000FFFFFFFF; // useless
-    // word = (word | (word << 16)) & 0x0000FFFF0000FFFF; // useless
-    // word = (word | (word << 8)) & 0x00FF00FF00FF00FF;
-    // word = (word | (word << 4)) & 0x0F0F0F0F0F0F0F0F;
-    // word = (word | (word << 2)) & 0x3333333333333333;
-    // word = (word | (word << 1)) & 0x5555555555555555;
-
-    // 21 bits coordinates for 3D array
-    //word &= 0x1FFFFF; // we only look at the first 21 bits
-    // word = (word | word << 32) & 0x1F00000000FFFF; // useless
-    // word = (word | word << 16) & 0x1F0000FF0000FF;
-    // word = (word | word << 8) & 0x100F00F00F00F00F;
-    // word = (word | word << 4) & 0x10C30C30C30C30C3;
-    // word = (word | word << 2) & 0x9249249249249249;
-
-    // 16-bits coordinates for 4D array
-    // word = (word | (word << 48)) & 0x000000000000FFFF; // useless
-    // word = (word | (word << 24)) & 0x000000FF000000FF;
-    // word = (word | (word << 12)) & 0x000F000F000F000F;
-    // word = (word | (word << 6)) & 0x0303030303030303;
-    // word = (word | (word << 3)) & 0x1111111111111111;
-
-    // 12-bits coordinates for 5D array
-    word = (word | (word << 32)) & 0x00FF00000000FF;
-    word = (word | (word << 16)) & 0xF0000F0000F0000F;
-    word = (word | (word << 8)) & 0x300C0300C0300C03;
-    word = (word | (word << 4)) & 0x1084210842108421;
-
-    return word;
-}
-
-fn generate_bitmasks(n_dim: u32) -> Vec<(u32, usize)> {
-    let mut bitmask:usize = 0;
-    let base: usize = 2;
-    let mut left_shift = n_dim - 1;
-    let mut num_group = n_dim;
+fn generate_magic_numbers(dimensions: usize) -> Vec<(u32, usize)> {
+    let mut num_bits = dimensions as u32;
+    let mut left_shift = num_bits - 1;
     let mut result: Vec<(u32, usize)> = Vec::new();
 
-    while num_group < usize::BITS {
+    while num_bits < usize::BITS {
+        let mut bitmask: usize = 0;
         for i in 0..usize::BITS {
-            if (i + num_group) % num_group < (num_group - left_shift) {
-                bitmask += base.pow(i);
+            if (i + num_bits) % num_bits < (num_bits - left_shift) {
+                bitmask += 2_usize.pow(i);
             }
         }
 
         result.push((left_shift, bitmask));
-        left_shift *= 2;
-        num_group *= 2;
-        bitmask = 0;
+        left_shift <<= 1;
+        num_bits *= 2;
     }
 
-    for i in result.iter(){
+    for i in result.iter() {
         print!("({}, {:X})", i.0, i.1);
     }
     println!("");
@@ -135,18 +111,51 @@ fn generate_bitmasks(n_dim: u32) -> Vec<(u32, usize)> {
     result
 }
 
-fn main() {
-    generate_bitmasks(5);
-}
 
+fn main() {}
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
+    fn test_generate_magic_numbers() {
+        assert_eq!(usize::BITS, 64);
+
+        let mut magic_numbers = generate_magic_numbers(2);
+        assert_eq!(magic_numbers.len(), 5);
+        assert_eq!(magic_numbers[0], (1, 0x5555555555555555));
+        assert_eq!(magic_numbers[1], (2, 0x3333333333333333));
+        assert_eq!(magic_numbers[2], (4, 0x0F0F0F0F0F0F0F0F));
+        assert_eq!(magic_numbers[3], (8, 0x00FF00FF00FF00FF));
+        assert_eq!(magic_numbers[4], (16, 0x0000FFFF0000FFFF)); // TODO: useless
+
+        magic_numbers = generate_magic_numbers(3);
+        assert_eq!(magic_numbers.len(), 5);
+        assert_eq!(magic_numbers[0], (2, 0x9249249249249249));
+        assert_eq!(magic_numbers[1], (4, 0x30C30C30C30C30C3)); // TODO: 0x1F00000000FFFF
+        assert_eq!(magic_numbers[2], (8, 0xF00F00F00F00F00F)); // TODO: 0x100F00F00F00F00F
+        assert_eq!(magic_numbers[3], (16, 0xFF0000FF0000FF)); // TODO: 0x1F0000FF0000FF
+        assert_eq!(magic_numbers[4], (32, 0xFFFF00000000FFFF)); // TODO: useless, 0x1F00000000FFFF
+
+        magic_numbers = generate_magic_numbers(4);
+        assert_eq!(magic_numbers.len(), 4);
+        assert_eq!(magic_numbers[0], (3, 0x1111111111111111));
+        assert_eq!(magic_numbers[1], (6, 0x0303030303030303));
+        assert_eq!(magic_numbers[2], (12, 0x000F000F000F000F));
+        assert_eq!(magic_numbers[3], (24, 0x000000FF000000FF));
+
+        magic_numbers = generate_magic_numbers(5);
+        assert_eq!(magic_numbers.len(), 4);
+        assert_eq!(magic_numbers[0], (4, 0x1084210842108421));
+        assert_eq!(magic_numbers[1], (8, 0x300C0300C0300C03));
+        assert_eq!(magic_numbers[2], (16, 0xF0000F0000F0000F));
+        assert_eq!(magic_numbers[3], (32, 0x00FF00000000FF));
+    }
+
+    #[test]
     fn test_morton_array() {
-        let shape = vec![4, 4, 4, 4, 4];
+        let shape = vec![4, 4, 4];
         let mut array: MortonArray<u32> = MortonArray::new(shape.clone());
 
         // Set and get values
