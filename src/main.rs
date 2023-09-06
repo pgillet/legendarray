bc84dd6f779b964fe12651714d2856400b8c81e8
Pascal Gillet <pascal.gillet@stack-labs.com>
2023-09-06 23:01:26 +0200

    Add log_base_2 function

--- src/main.rs
+++ src/main.rs
@@ -17,6 +17,23 @@ impl MortonLayout {
         word
     }
 
+    /// Find the log base 2 of an integer with the MSB N set
+    /// in O(N) operations (the obvious way)
+    fn log_base_2(i: usize) -> u32 {
+        let mut v: usize = i;
+        let mut r: u32 = 1;
+        loop {
+            v >>= 1;
+
+            if v == 0 {
+                break;
+            }
+
+            r += 1;
+        }
+        r
+    }
+
     fn generate_magic_numbers(dimensions: usize) -> Vec<(u32, usize)> {
         let mut num_bits = dimensions as u32;
         let mut left_shift = num_bits - 1;
@@ -134,6 +151,14 @@ fn main() {}
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn test_log_base_2() {
+        assert_eq!(MortonLayout::log_base_2(5), 3);
+        assert_eq!(MortonLayout::log_base_2(16), 5);
+        assert_eq!(MortonLayout::log_base_2(77), 7);
+        assert_eq!(MortonLayout::log_base_2(128), 8);
+    }
 
     #[test]
     fn test_generate_magic_numbers() {
